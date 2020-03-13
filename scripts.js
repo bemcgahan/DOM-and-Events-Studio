1adf68825b8f5217acd23f82dd1e3ed5a4e4331c
Brenna McGahan <brenna.degan@gmail.com>
2020-03-12 19:37:01 -0500

    finished steps 1 and 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,17 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+//load event handler
+window.addEventListener("load", function (){
+    window.confirm("Confirm that the shuttle is ready for takeoff.")
+
+    this.document.getElementById("flightStatus").innerHTML = "Shuttle in flight";
+
+    this.document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+
+    let currHeight = this.document.getElementById('spaceShuttleHeight').innerHTML;
+
+    let newHeight = Number(currHeight) + 10000;
+
+    this.document.getElementById('spaceShuttleHeight').innerHTML = newHeight;
+});
