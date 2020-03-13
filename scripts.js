9a51fed1b241cdfd1cfcf827cf9e81f1ea32ffcb
Brenna McGahan <brenna.degan@gmail.com>
2020-03-12 20:03:47 -0500

    stuck on step 3

--- scripts.js
+++ scripts.js
@@ -14,4 +14,28 @@ window.addEventListener("load", function (){
     let newHeight = Number(currHeight) + 10000;
 
     this.document.getElementById('spaceShuttleHeight').innerHTML = newHeight;
+
+    function youLand () {
+    
+        let result = window.alert("The shuttle is landing. Landing gear engaged.");
+        if (result){
+            this.document.getElementById("flightStatus").innerHTML = "The shuttle has landed";
+            this.document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        }
+    }
+
+    function abortMission (){
+        window.confirm("Confirm that you want to abort the mission")
+
+        do
+    }
+    
+    let landButton = document.getElementById("landing");
+    let abortButton = document.getElementById('missionAbort');
+    
+    landButton.addEventListener("click", youLand);
+    abortButton.addEventListener("click", abortMission);
+
+
 });
+
